b58d9f385b1325c8b6a5c4ef33dd4116900b7c79
sabyasachi <sabyasachihyd2003@gmail.com>
2025-02-09 22:51:34 +0530

    Create top-offset-fetch.sql
    top-offset

---
+++ mssql/t-sql/top-offset-fetch.sql
@@ -0,0 +1,22 @@
+-- SQL Server views
+-- Author: Sabyasachi Mitra
+-- Date: 02/09/2025
+-- Description: Demonstrate TOP and OFFSET-FETCH--
+--
+USE AdventureWorks2022;
+SELECT   FirstName,
+         LastName
+FROM     Person.Person
+ORDER BY LastName
+--
+SELECT   TOP (5) FirstName,
+         LastName
+FROM     Person.Person
+ORDER BY LastName;
+--
+-- DISTINCT with TOP: DISTIINCT is applied before TOP
+--
+SELECT DISTINCT TOP(10) FirstName,
+         LastName
+FROM     Person.Person
+ORDER BY LastName;
